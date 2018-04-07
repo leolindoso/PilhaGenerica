c64bc8fd842973f1db91c058418408f280456f74
Leonardo Lindoso <leo7lindoso@hotmail.com>
2018-04-07 14:55:12 -0300

    Criando Parte do Menu das Opções

--- PilhaInt/main.c
+++ PilhaInt/main.c
@@ -4,5 +4,50 @@
 
 int main(){
 
-    st
+    stack *stk;
+    int modo = 100,tam;
+
+    while(modo == 100){
+        printf("1-Cria Pilha\n2-Adiciona Elemento à Pilha\n3-Checkar Ultimo Elemento da Pilha"
+                       "\n4-Retirar Ultimo Elemento da Pilha\n5-Destruir Pilha e Sair\n0-Sair\n");
+        scanf("%i",&modo);
+        if (modo == 1){
+            if(stk != NULL){
+                printf("Informe a quantidade de elementos que quer botar na pilha: \n");
+                scanf("%i",&tam);
+                stk = stkCreate(tam);
+                if (stk != NULL){
+                    printf("Pilha criada com Sucesso!\n");
+                }else{
+                    printf("Falha ao criar Pilha!\n");
+                }
+            }else{
+                printf("Ja ta criado\n");
+            }
+            modo = 100;
+        }
+        if(modo == 2){
+            int x;
+            printf("Informe qual numero quer colocar na Pilha: \n");
+            scanf("%i",&x);
+            int flag = stkPush(stk,(void*)x);
+            if (flag == 1){
+                printf("Elemento Inserido com Sucesso!\n");
+            }else{
+                printf("Falha ao Inserir Elemento!\n");
+            }
+            modo = 100;
+        }
+        if(modo == 3){
+            char *topaux;
+            topaux = (char*)stkTop(stk);
+            if(topaux != NULL){
+
+            }else{
+                printf("Falha ao Checar Ultimo Elemento!\n");
+            }
+            //printf("O Ultimo Numero da Pilha é %s",topaux);
+        }
+        modo = 100;
+    }
 }
